dca40249c35952b01ec99edd9a8e8ecf5097895f
YukiOkazaki <kapi_desu@outlook.jp>
2018-12-13 12:05:52 +0900

    コメントアウトの追加 色の追加

--- Quarto.java
+++ Quarto.java
@@ -5,12 +5,11 @@ import java.util.*;
 
 class BoardObservable extends Observable { 
     private int b[] = new int[16];  //board
-    private int koma[] = //{1,2,3,5,6,7,10,14,15,21,30,35,42,70,105,210};
-    {1,5,7,35,2,10,14,70,3,15,21,105,6,30,42,210};
+    private int koma[] = {1,5,7,35,2,10,14,70,3,15,21,105,6,30,42,210};
     private int sp; //select position 
-    private int playernum;
-    private int situation; //0が選択画面、1が盤面に置く
-    private int selectplace;
+    private int playernum;        //操作するのが何Pなのか1or2
+    private int situation;        //0が選択画面、1が盤面に置く
+    private int selectplace;      //どの場所からその駒を持って来たかを保存する変数
     public void initialize_board(){
 	sp = 0;
 	playernum = 2;
@@ -91,7 +90,7 @@ class BoardObservable extends Observable {
 
 }
 
-class BoardObserver extends JPanel implements Observer {
+class BoardObserver extends JPanel implements Observer {                       //observer側のすべての親クラス
     protected BoardObservable BO;
     protected JLabel label;
     protected int val;
@@ -99,13 +98,13 @@ class BoardObserver extends JPanel implements Observer {
     protected int situation;
     public BoardObserver(BoardObservable observable){
 	BO = observable;
-	BO.addObserver(this);
+	BO.addObserver(this);                                                  //observerに登録
 	label = new JLabel();
-	this.setLayout(new BorderLayout());
-	this.add(label, BorderLayout.CENTER);
-	label.setHorizontalAlignment(JLabel.CENTER);
-	label.setVerticalAlignment(JLabel.CENTER);
-	label.setFont(new Font(Font.SANS_SERIF,Font.BOLD,18)); 
+	this.setLayout(new BorderLayout());                                    //panelは通常FlowLayoutなのでBorderLayoutに変更
+	this.add(label, BorderLayout.CENTER);                                  //panel内に表示されるラベルの追加
+	label.setHorizontalAlignment(JLabel.CENTER);                           //ラベルの位置は水平方向で中心
+	label.setVerticalAlignment(JLabel.CENTER);                             //ラベルの位置は垂直方向で中心
+	label.setFont(new Font(Font.SANS_SERIF,Font.BOLD,16));                 //ラベルのフォントの設定
     }
     
     public void update(Observable o, Object arg){}
@@ -113,58 +112,66 @@ class BoardObserver extends JPanel implements Observer {
 }
 
 class Select extends BoardObserver {
-    private JLabel playerlabel;
+    private JLabel playerlabel;                                                //何Pが操作するべきなのか等の情報を表示するラベル
+    private String maincolor, opponentcolor;                                   //IP,2Pそれぞれの色を指定する
     public Select(BoardObservable observable){
-	super(observable);
+	super(observable);                                                     //親のコンストラクタを一度呼びだす
 	playerlabel = new JLabel();
-	this.add(playerlabel, BorderLayout.SOUTH);
+	this.add(playerlabel, BorderLayout.SOUTH);                             //playerlabelを下に配置
 	val = BO.get_selectpiece();
-	label.setText(String.valueOf(val));
-	playerlabel.setHorizontalAlignment(JLabel.CENTER);
+	label.setText(String.valueOf(val));                                    //なくてもよい？
+	playerlabel.setHorizontalAlignment(JLabel.CENTER);                     //plyaerlabelの水平方向の位置は中心
 	playerlabel.setText("");
-	playerlabel.setFont(new Font(Font.SANS_SERIF,Font.BOLD,18));
-	BO.initialize_board();
+	playerlabel.setFont(new Font(Font.SANS_SERIF,Font.BOLD,16));           //フォント設定
+	BO.initialize_board();                                                 //一度初期化(updateを呼ぶため)
     }
     
     @Override
-    public void update(Observable o, Object arg){
-	val = BO.get_selectpiece();
-	if(val != 0){
+    public void update(Observable o, Object arg){                              //BoardObserverを継承しているのでupdateをoverride
+	val = BO.get_selectpiece();                                            //選ばれた駒の値をvalに入れる
+	if(val != 0){                                                          //選ばれた駒が有効（値が0でない）場合は選ばれた値の画像を表示
 	    label.setText("");
-	    ImageIcon icon = new ImageIcon("./img/"+val+".png");
+	    ImageIcon icon = new ImageIcon("./img/"+val+".png");               //iconに選ばれた値の画像を読み込む
 	    Image smallimg = icon.getImage().getScaledInstance((int)(icon.getIconWidth()*1.0),(int)(icon.getIconHeight()*1.0),Image.SCALE_DEFAULT);
-	    ImageIcon smallicon = new ImageIcon(smallimg);
-	    label.setIcon(smallicon);
+	    ImageIcon smallicon = new ImageIcon(smallimg);                     //大きさを調整したsmallimageをsmalliconに読み込ませる
+	    label.setIcon(smallicon);                                          //ラベルに配置する
 	} else {
-	    label.setIcon(null);
+	    label.setIcon(null);                                               //もし有効な駒でなければ何も表示しない
 	    label.setText(String.valueOf(val));
 	}
-	playernum = BO.get_playernum();
-	situation = BO.get_situation();
-	if(situation == 0)
-	    playerlabel.setText("<html>"+playernum+"Pが相手の駒を選んでください<br>そろっていればQuarto!と宣言してください</html>");
+	playernum = BO.get_playernum();                                        //何Pの番なのかをplayernumに入れる
+	if(playernum == 1){                                                    //playernumに応じて色を決める
+	    maincolor = "blue";
+	    opponentcolor = "red";
+	} else {
+	    maincolor = "red";
+	    opponentcolor = "blue";
+	}
+	situation = BO.get_situation();                                        //選択画面なのか、盤面に置く画面なのかをsituationとして受け取る
+	if(situation == 0)                                                     //situationに応じてメッセージを表示する
+	    playerlabel.setText("<html><span style='font-size:24pt; color: "+maincolor+";'>"+playernum+"P</span>が相手の駒を選んでください<br>そろっていれば<span style='font-size:26pt; color: #FFCC00;'>Quarto!</span>と宣言してください</html>");
 	if(situation == 1)
-	    playerlabel.setText("<html>"+playernum+"Pが駒を盤面に置いてください<br>または"+(playernum %2 +1)+"Pが相手の駒を選びなおしてください</html>");
+	    playerlabel.setText("<html><span style='font-size:24pt; color: "+maincolor+";'>"+playernum+"P</span>が駒を盤面に置いてください<br>または<span style='font-size:24pt; color: "+opponentcolor+";'>"+(playernum %2 +1)+"P</span>が相手の駒を選びなおしてください</html>");
     }
     
 
 }
 
-class Battle extends BoardObserver implements MouseListener {   //BattleはBoardObserverを継承
+class Battle extends BoardObserver implements MouseListener {   //BattleはBoardObserverを継承, MouseListenerを追加
     private int place;
     public Battle(BoardObservable observable, int place){
-	super(observable);
+	super(observable);                                      //親のコンストラクタを一度呼び出す
 	this.place = place;
-	this.addMouseListener(this);
+	this.addMouseListener(this);                            //MouseListenerを追加
         val = BO.get_piece(place);
-	label.setText(String.valueOf(val));	
+	label.setText(String.valueOf(val));	                //なくてもよい？
     }
     
-    @Override                                                //updateをOverrideでBattle用に変更
+    @Override                                                   //updateをOverrideでBattle用に変更
     public void update(Observable o, Object arg){
-        val = BO.get_piece(place);
-	label.setText(String.valueOf(val));
-	if(val != 0){
+        val = BO.get_piece(place);                              //盤面に置かれるべき駒の値をvalに入れる
+	label.setText(String.valueOf(val));                     //なくてもよい？
+	if(val != 0){                                           //駒が有効であれば値の画像を表示 表示方法はSelectと同様
 	    label.setText("");
 	    ImageIcon icon = new ImageIcon("./img/"+val+".png");
 	    Image smallimg = icon.getImage().getScaledInstance((int)(icon.getIconWidth()*0.7),(int)(icon.getIconHeight()*0.7),Image.SCALE_DEFAULT);
@@ -176,37 +183,37 @@ class Battle extends BoardObserver implements MouseListener {   //BattleはBoard
 	}
     }
     
-    public void mouseClicked(MouseEvent e){
-	val = BO.get_selectpiece();
-	if(val == 0)
+    public void mouseClicked(MouseEvent e){                     //マウスで盤面をクリックされた時の動作
+	val = BO.get_selectpiece();                             //どの駒が選ばれているかをvalに入れる
+	if(val == 0)                                            //何も選ばれていなければそのまま終了
 	    return;
-	if(BO.get_piece(place) != 0)
+	if(BO.get_piece(place) != 0)                            //盤面にすでに置かれている場合はそのまま終了
 	    return;
-	BO.set_situation(0);
-	BO.set_piece(val, place);
-	setBackground(null);
+	BO.set_situation(0);                                    //situationを0（選択画面）に変更
+	BO.set_piece(val, place);                               //盤面に選ばれた駒を置く
+	setBackground(null);                                    //背景（青）を消す
     }
     public void mousePressed(MouseEvent e) { }
     public void mouseReleased(MouseEvent e){ }
     public void mouseEntered(MouseEvent e) { 
-	this.setBackground(new Color(0,0,255,50));
+	this.setBackground(new Color(0,0,255,50));              //マウスがこのpanelに入った場合背景を青にする
     }
     public void mouseExited(MouseEvent e)  {
-	this.setBackground(null);
+	this.setBackground(null);                               //マウスがこのpanelから出た場合背景を消す
     }
     
 }
 
-class Standby extends BoardObserver implements MouseListener {                                  //StandbyはBoardObserverを継承
+class Standby extends BoardObserver implements MouseListener {  //StandbyはBoardObserverを継承, MouseListenerを追加
     private int tmp;
     private int place;
     private int selectplace;
-    public Standby(BoardObservable observable, int place){
-	super(observable);
+    public Standby(BoardObservable observable, int place){            
+	super(observable);                                      //親のコンストラクタを一度呼びだす
 	this.place = place;
-	this.addMouseListener(this);
-	val = BO.get_standbypiece(place);
-	if(val != 0){
+	this.addMouseListener(this);                            //MouseListenerを追加
+	val = BO.get_standbypiece(place);                       //現在ある駒の値をvalに入れる
+	if(val != 0){                                           //もし値があればその値の画像を表示 表示方法は同様                                  
 	    label.setText("");
 	    ImageIcon icon = new ImageIcon("./img/"+val+".png");
 	    Image smallimg = icon.getImage().getScaledInstance((int)(icon.getIconWidth()*0.4),(int)(icon.getIconHeight()*0.4),Image.SCALE_DEFAULT);
@@ -218,42 +225,42 @@ class Standby extends BoardObserver implements MouseListener {                  
 	}
     }
     
-    @Override                                                                                    //updateをOverrideでStandby用に変更
+    @Override                                                   //updateをOverrideでStandby用に変更
     public void update(Observable o, Object arg){
-	val = BO.get_standbypiece(place);
-	if(val != 0){
+	val = BO.get_standbypiece(place);                       //現在ある駒の値をvalに入れる
+	if(val != 0){                                           //駒が有効であればその値の画像を表示 表示方法は同様
 	    label.setText("");
 	    ImageIcon icon = new ImageIcon("./img/"+val+".png");
 	    Image smallimg = icon.getImage().getScaledInstance((int)(icon.getIconWidth()*0.4),(int)(icon.getIconHeight()*0.4),Image.SCALE_DEFAULT);
 	    ImageIcon smallicon = new ImageIcon(smallimg);
 	    label.setIcon(smallicon);
-	} else {
+	} else {                                                //駒がなければ何も表示しない
 	    label.setIcon(null);
-	    label.setText(String.valueOf(val));
+	    label.setText(String.valueOf(val));                 //なくてもよい？
 	}
     }
     
-    public void mouseClicked(MouseEvent e){
-	val = BO.get_standbypiece(place);
-	tmp = BO.get_selectpiece();
-	if(val == 0)
+    public void mouseClicked(MouseEvent e){                     //マウスで置き駒をクリックされた時の動作
+	val = BO.get_standbypiece(place);                       //現在ある駒の値をvalに入れる
+	tmp = BO.get_selectpiece();                             //現在選ばれている(Select)にある駒の値をtmpに入れる
+	if(val == 0)                                            //駒がなければそのまま終了
 	    return;
-	if(tmp == 0)
+	if(tmp == 0)                                            //駒が選ばれていなければ、playernumを変更
 	    BO.set_playernum(BO.get_playernum()%2 + 1);
-	BO.set_situation(1);
-	BO.set_selectpiece(val, place);
-	if(tmp != 0){
-	    selectplace = BO.get_selectplace();
-	    BO.set_standbypiece(tmp, selectplace);
+	BO.set_situation(1);                                    //situaitonを盤面におく状態（1）に変更
+	BO.set_selectpiece(val, place);                         //valを選びSelectにセットする
+	if(tmp != 0){                                           //選びなおしの動作
+	    selectplace = BO.get_selectplace();                 //selectplaceに前回どこの場所から持って来たのかという情報を入れる
+	    BO.set_standbypiece(tmp, selectplace);              //前回の場所にtmpを戻す
 	}
-	BO.set_selectplace(place);
-	setBackground(null);
+	BO.set_selectplace(place);                              //今回どこの場所から持って来たのかという情報を登録する
+	setBackground(null);                                    //背景(青)を消す
     }
     
     public void mousePressed(MouseEvent e) { }
     public void mouseReleased(MouseEvent e){ }
     public void mouseEntered(MouseEvent e) { 
-	this.setBackground(new Color(0,0,255,50));
+	this.setBackground(new Color(0,0,255,50));              //マウスの動作はBattleと同様
     }
     public void mouseExited(MouseEvent e)  {
 	this.setBackground(null);
@@ -261,24 +268,28 @@ class Standby extends BoardObserver implements MouseListener {                  
     
 }
 
-class CompleteButton extends BoardObserver implements ActionListener {
+class CompleteButton extends BoardObserver implements ActionListener {   //BoardObsever継承, ActionListenerを追加
     private JButton complete;
+    private String maincolor;
     public CompleteButton(BoardObservable observable){
-	super(observable);
-	complete = new JButton("Quarto!");
-	this.setLayout(new GridLayout(2,1));
-	this.add(complete, BorderLayout.EAST);
-	
-	//	label.setVerticalAlignment(JLabel.N);
-	complete.addActionListener(this);
-	BO.initialize_board();
+	super(observable);                                      //親コンストラクタを呼び出す
+	complete = new JButton("<html><span style='font-size:50pt; color: #FFCC00;' >Quarto!</span></html>"); //ボタンを作成
+	complete.setFocusPainted(false);                        //ボタンの枠線を消す
+	this.setLayout(new GridLayout(2,1));                    //panel内をGirdLayoutにし、縦に二分割
+	this.add(complete);                                     //ボタンを下に追加;
+	complete.addActionListener(this);                       //ActionListenerを追加
     }
-    public void actionPerformed(ActionEvent e) {
-	situation = BO.get_situation();
-	playernum = BO.get_playernum();
-	if(situation == 0){
-	    if(BO.is_complete() == 1){
-		label.setText("<html>揃っています<br>"+playernum+"Pの勝ちです</html>");
+    public void actionPerformed(ActionEvent e) {                //ボタンが押された時の動作
+	situation = BO.get_situation();                         //situationを入手
+	playernum = BO.get_playernum();                         //playernumを入手
+	if(playernum == 1){                                     //playernumに応じて色を決める
+	    maincolor = "blue";
+	} else {
+	    maincolor = "red";
+	}
+	if(situation == 0){                                     //判定できるのは盤面に置いた後のみ(situaitonが0)
+	    if(BO.is_complete() == 1){                          //is_completeが1ならば揃っている
+		label.setText("<html>揃っています<br><span style='font-size:30pt; color:"+ maincolor+";'>"+playernum+"P</span>の勝ちです</html>");
 	    } else{
 		label.setText("揃っていません");
 	    }
